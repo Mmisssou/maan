3b9ff508e4a723150cbf65615c625c0362ee6189
Mmisssou <108935725+Mmisssou@users.noreply.github.com>
2022-07-27 14:38:15 +0200

    Update maan.py

--- maan.py
+++ maan.py
@@ -42,4 +42,34 @@ df_prediction_by_id = df_clients_to_predict[df_clients_to_predict["SK_ID_CURR"] 
 
 
 
+@app.get("/clients/{sk_ids}")
+async def client_details(sk_ids: int):
+    """ 
+    EndPoint to get client's detail 
+    """ 
+
+    clients_id = df_clients_to_predict["SK_ID_CURR"].tolist()
+
+    if sk_ids not in clients_id:
+        raise HTTPException(status_code=404, detail="client's id not found")
+    else:
+        # Filtering by client's id
+        df_by_id = df_clients_to_predict[COLUMNS][df_clients_to_predict["SK_ID_CURR"] == sk_ids]
+        idx = df_clients_to_predict[df_clients_to_predict["SK_ID_CURR"]==sk_ids].index[0]
+
+        for col in df_by_id.columns:
+            globals()[col] = df_by_id.iloc[0, df_by_id.columns.get_loc(col)]
+        
+        client = {
+            "clientId" : int(SK_ID_CURR),
+            "gender" : "Man" if int(CODE_GENDER) == 0 else "Woman",
+            "age" : calculate_years(int(DAYS_BIRTH)),
+            "antiquity" : calculate_years(int(DAYS_REGISTRATION)),
+            "credit" : float(AMT_CREDIT),
+            "anualIncome" : float(AMT_INCOME_TOTAL),
+            "source2" : float(EXT_SOURCE_2),
+            "source3" : float(EXT_SOURCE_3),
+        }
+
+    return client
 
