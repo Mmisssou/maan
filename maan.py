bb2be8481a0709f98b08f27c0dc82a5726f6d07c
Mmisssou <108935725+Mmisssou@users.noreply.github.com>
2022-07-27 13:50:58 +0200

    Update maan.py

--- maan.py
+++ maan.py
@@ -4,6 +4,16 @@
 # In[1]:
 
 
+import os
+import json 
+import pandas as pd
+import numpy as np
+import lightgbm as lgb
+from lightgbm import LGBMClassifier
+import matplotlib.pyplot as plt
+import joblib
+from fastapi import FastAPI, Depends, HTTPException, status
+import sys
 from fastapi import FastAPI
 
 app = FastAPI( title="scoring crédit",
